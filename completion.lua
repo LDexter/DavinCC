7a8cf42ec167b18c436e4e54367df1a6cbb98527
Levi Miller <ldextermiller@gmail.com>
2023-01-14 17:12:24 +1000

    Automation of contextual summaries and str funcs.

--- completion.lua
+++ completion.lua
@@ -2,19 +2,37 @@ local completion = {}
 
 -- Import openai API
 local openai = require("../openai-lua/openai")
+local quill = require("quill")
 
 
 -- Request text from Davinci, given provided prompt, temperature, and maximum tokens
 function completion.request(prompt, temp, tokens)
-    local cmplData = openai.complete("text-davinci-003", prompt, temp, tokens)
+    local cmplJSON = openai.complete("text-davinci-003", prompt, temp, tokens)
+
+    -- Unserialise into lua object/table
+    local cmplOut = textutils.unserialiseJSON(cmplJSON)
+    -- Test choices for contextless summaries
+    for idx, choice in pairs(cmplOut["choices"]) do
+        -- Find openg space to indicate lack of context
+        local summStart = string.find(choice["text"], " ")
+        if summStart == 1 then
+            -- Capitolise first letter of prompt
+            prompt = quill.firstUpper(prompt)
+
+            -- Concatonate prompt as prefix to response and semicolon as suffix to summary
+            local summEnd = string.find(choice["text"], "\n") or string.len(choice["text"]) + 1
+            cmplOut["choices"][idx]["text"] = prompt .. quill.insert(choice["text"], ";", summEnd - 1)
+        end
+    end
+
+    cmplJSON = textutils.serialiseJSON(cmplOut)
 
     -- Storing response locally for later access --! Overwriting
     local cmplFile = fs.open("/DavinCC/cmpl.json", "w")
-    cmplFile.write(cmplData)
+    cmplFile.write(cmplJSON)
     cmplFile.close()
 
-    -- Return as lua object/table
-    return textutils.unserialiseJSON(cmplData)
+    return cmplOut
 end
 
 
---
+++ quill.lua
@@ -0,0 +1,14 @@
+local quill = {}
+
+
+function quill.insert(str1, str2, pos)
+    return str1:sub(1,pos) .. str2 .. str1:sub(pos+1)
+end
+
+
+function quill.firstUpper(str)
+    return (str:gsub("^%l", string.upper))
+end
+
+
+return quill
