4acbeb3a6a1475153d1c4baff3dc0c8285206b06
Levi Miller <ldextermiller@gmail.com>
2023-01-26 21:08:00 +1000

    Made "da.lua" program, just for conversations

--- da.lua
+++ da.lua
@@ -3,36 +3,43 @@ local completion = require("completion")
 local quill = require("quill")
 
 
--- Determine personality and risk
-local personality = "standard"
-local risk = 1
+-- User input for risk and personality
+local personality, risk = ...
+personality = personality or "standard"
+
+-- Input testing for non-number
+if type(risk) ~= "number" then
+    risk = 0
+end
+-- Input testing for out of range
+if risk < 0 then
+    risk = 0
+elseif risk > 1 then
+    risk = 1
+end
+
+
 -- Select greeting file based on personality
 personality = quill.firstUpper(personality)
 local greetFile = "/DavinCC/greetings/greet" .. personality .. ".txt"
 
 
--- Generate completion request from prompt, with a risk temperature and maximum token limit
-completion.request("Say this is a test", 0, 10)
-
-
--- Source locally stored response and print text from first choice
-local last = completion.last()
-last = last["choices"][1]["text"]
-print(last .. "\n")
-
-
 -- Initiate a conversation
 completion.greet(greetFile)
-local start = quill.scribe(greetFile, "r")
-print(start .. "\n")
+quill.scribe(greetFile, "r")
+
+
+-- Start with reply to "hello" prompt
+local cont = completion.continue("hello", risk, 200, 5)
+print(cont["choices"][1]["text"])
 
 
 -- Continue the conversation indefinately
 local prompt
-local cont
 while true do
     print("\n")
     prompt = read()
+    print("\n")
     -- Continue with prompt (user input), risk (0-1), token limit (max per reply), cutoff (how many replies to remember)
     cont = completion.continue(prompt, risk, 200, 5)
     print(cont["choices"][1]["text"])
