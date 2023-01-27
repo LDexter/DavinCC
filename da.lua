28e8c33edd0682e56d860d087cd96e458bc214a9
Levi Miller <ldextermiller@gmail.com>
2023-01-27 22:26:52 +1000

    New "none" personality option for quick prompts

--- da.lua
+++ da.lua
@@ -6,6 +6,7 @@ local quill = require("quill")
 -- User input for risk and personality
 local personality, risk, cutoff = ...
 personality = personality or "standard"
+personality = string.lower(personality)
 risk = tonumber(risk)
 
 -- Input testing for non-number
@@ -27,33 +28,47 @@ elseif cutoff > 42 then
     cutoff = 42
 end
 
-
--- Printing chosen arguments
-print("Personality: \"" .. personality .. "\" Risk: " .. risk .. " Cutoff: " .. cutoff)
-
-
--- Select greeting file based on personality
-personality = quill.firstUpper(personality)
-local greetFile = "/DavinCC/greetings/greet" .. personality .. ".txt"
-
-
--- Initiate a conversation
-completion.greet(greetFile)
-quill.scribe(greetFile, "r")
-
-
--- Start with reply to "hello" prompt
-local cont = completion.continue("hello", risk, 200, 5)
-print(cont["choices"][1]["text"])
-
-
--- Continue the conversation indefinately
 local prompt
-while true do
-    print("\n")
+local cont
+
+
+-- Quick and flavourless request
+if personality == "none" then
+    print("Personality: \"" .. personality .. "\"\n")
     prompt = read()
-    print("\n")
-    -- Continue with prompt (user input), risk (0-1), token limit (max per reply), cutoff (how many replies to remember)
-    cont = completion.continue(prompt, risk, 200, 5)
+    -- Complete prompt (user input), risk (0-1), token limit
+    cont = completion.request(prompt, risk, 200)
+    print(cont["choices"][1]["text"] .. "\n")
+
+
+-- Otherwise, conduct conversation with chosen personality
+else
+    -- Printing chosen arguments
+    print("Personality: \"" .. personality .. "\" Risk: " .. risk .. " Cutoff: " .. cutoff)
+
+
+    -- Select greeting file based on personality
+    personality = quill.firstUpper(personality)
+    local greetFile = "/DavinCC/greetings/greet" .. personality .. ".txt"
+
+
+    -- Initiate a conversation
+    completion.greet(greetFile)
+    quill.scribe(greetFile, "r")
+
+
+    -- Start with reply to "hello" prompt
+    cont = completion.continue("hello", risk, 200, cutoff)
     print(cont["choices"][1]["text"])
+
+
+    -- Continue the conversation indefinately
+    while true do
+        print("\n")
+        prompt = read()
+        print("\n")
+        -- Continue with prompt (user input), risk (0-1), token limit (max per reply), cutoff (how many replies to remember)
+        cont = completion.continue(prompt, risk, 200, cutoff)
+        print(cont["choices"][1]["text"])
+    end
 end
